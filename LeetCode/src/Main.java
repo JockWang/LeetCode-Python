e53d27b58339742f3798f53e7eb669f5e1fbeaf2
jockwang <jockmail@126.com>
2019-06-13 08:17:50 +0800

    update 54

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -2,14 +2,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String args[]){
-        System.out.println(new Solution43().multiply("123456789","987654321"));
+//        System.out.println(new Solution43().multiply("123456789","987654321"));
+        int [][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        print(new Solution54().spiralOrder(matrix));
     }
-    public static void print(List<List<Integer>> list){
-        for(List li:list){
-            for(Object i:li){
-                System.out.print((int)i);
-            }
-            System.out.println("");
+//    public static void print(List<List<Integer>> list){
+//        for(List li:list){
+//            for(Object i:li){
+//                System.out.print((int)i);
+//            }
+//            System.out.println("");
+//        }
+//    }
+    public static void print(List<Integer> list){
+        for(Integer i:list){
+            System.out.print(i+" ");
         }
     }
 }
