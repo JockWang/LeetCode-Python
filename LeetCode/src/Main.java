2bca0d650b9dfcd3997ee3138052c55ef8db16c3
jockwang <jockmail@126.com>
2019-06-15 00:44:15 +0800

    add 56

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -2,8 +2,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String args[]){
-        int [] jump = {3,2,1,0,4};
-        System.out.println(new Solution55().canJump(jump));
+        int [][] input = {{3,4},{2,3}};
+        print(new Solution56().merge(input));
+//        int [] jump = {3,2,1,0,4};
+//        System.out.println(new Solution55().canJump(jump));
 //        System.out.println(new Solution43().multiply("123456789","987654321"));
 //        int [][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 //        print(new Solution54().spiralOrder(matrix));
@@ -16,9 +18,14 @@ public class Main {
 //            System.out.println("");
 //        }
 //    }
-    public static void print(List<Integer> list){
-        for(Integer i:list){
-            System.out.print(i+" ");
+//    public static void print(List<Integer> list){
+//        for(Integer i:list){
+//            System.out.print(i+" ");
+//        }
+//    }
+    public static void print(int[][] list){
+        for(int i = 0;i < list.length;i ++){
+            System.out.print("["+list[i][0]+","+list[i][1]+"]");
         }
     }
 }
