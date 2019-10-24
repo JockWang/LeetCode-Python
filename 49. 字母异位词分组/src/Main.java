e7b92fc7bea950565582f3a40396e3c40db7f50d
jockwang <jockmail@126.com>
2019-10-24 22:32:31 +0800

    update

--- 49. 字母异位词分组/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String args[]){
-        String[] strlist = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        new Solution().groupAnagrams(strlist);
-    }
-}
--- LeetCode/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String args[]){
-        System.out.println("OK");
-//        Solution1175 s = new Solution1175();
-//        System.out.println(s.numPrimeArrangements(5));
-    }
-}
